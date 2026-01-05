6be1428774a88407d31ba25b9b8f6838153f4d2b
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2026-01-05 18:57:46 -0300

    feat: memória adicionada

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
-from ast import parse
-from html import parser
 import os
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate , MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
 load_dotenv()
+
+store = {}
+
+def get_session_history(session_id: str):
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 def iniciar_chatbot():
     print("Iniciando o chatbot... (digite 'sair' para encerrar)")
@@ -19,13 +26,24 @@ def iniciar_chatbot():
     )
 
     prompt = ChatPromptTemplate.from_messages([
-        ("system", "Você é um assistente virtual útil. Responda apenas em português."),
+        ("system", "Você é um assistente útil e especialista em programação Python."),
+        ("system", "Responda de forma concisa."),
+        MessagesPlaceholder(variable_name="history"),
         ("user", "{input}")
     ])
 
     parser = StrOutputParser()
 
     chain = prompt | llm | parser
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+
+    session_id = "sessao_usuario_1"
 
     while True:
         try:
@@ -37,8 +55,10 @@ def iniciar_chatbot():
 
             print("Gemini: ", end="", flush=True)
             
-            # O streaming também funciona nativamente
-            for chunk in chain.stream({"input": user_input}):
+            for chunk in chain_with_history.stream(
+                {"input": user_input},
+                config={"configurable": {"session_id": session_id}}
+            ):
                 print(chunk, end="", flush=True)
             print()
 
